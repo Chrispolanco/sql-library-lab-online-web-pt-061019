cab4191e9c4620f5a6b312c60f0dfb363c8a256e
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-26 01:48:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,5 +21,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  ""
+  "SELECT characters.name, COUNT(*) AS num_books FROM character_books JOIN characters
+  WHERE characters.id = character_books.character_id GROUP BY characters.name ORDER BY num_books DESC;"
 end
